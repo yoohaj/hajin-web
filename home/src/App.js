0d544dd6ea65ee1520a3e69126678d4769792090
yoohaj <dbgkwls10@naver.com>
2022-10-21 20:02:58 +0900

    first commit

---
+++ home/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Top">
+        <a href="#">logo</a>
+        <ul className="Main_menu">
+          <a href="#"><li>ABout</li></a>
+          <a href="#"><li>My skills</li></a>
+          <a href="#"><li>Project</li></a>
+          <a href="#"><li>blog</li></a>
+          <a href="#"><li>blog</li></a>
+          <a href="#"><li>blog</li></a>
+
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
